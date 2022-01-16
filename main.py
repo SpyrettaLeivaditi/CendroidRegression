c423350ec32dbe0bfe2edb1110fb59fadcffdc62
spyretta.leiv <spyretta.leiv@gmail.com>
2022-01-16 13:16:18 +0100

    implement MLP regression and fix plots (TODO: change the  titles of the plots)

--- main.py
+++ main.py
@@ -6,25 +6,34 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics import ConfusionMatrixDisplay
 from sklearn.model_selection import train_test_split, cross_validate
 from sklearn.preprocessing import MinMaxScaler
+from sklearn.neural_network import MLPRegressor
 
 # Global counter for figure enumeration
 i = 0
 
 
-def confusion_matrix_scorer(model, x, y):
-    """ Plot the confusion matrix
+def prediction_plot_scorer(model, x, y):
+    """ Plot predictions
     :param model: the linear regression model
     :param x: features
-    :param y: labels
+    :param y: labels(not used)
     :return: I return the beloved 42, just to return the proper data type
     """
-    ConfusionMatrixDisplay.from_estimator(model, x, y)
+    x_feature = x[:, :1]
+    x_plot = x_feature.reshape(len(x_feature), 1)
+    # Plot outputs
     global i
     i += 1
+
+    plt.scatter(x_plot, y, color='black')
     plt.title(f'Figure {i}')
+    plt.xlabel('Size')
+    plt.ylabel('Price')
+    plt.xticks(())
+    plt.yticks(())
+    plt.plot(x_plot, model.predict(x), color='red', linewidth=3)
     plt.show()
     return 42
 
@@ -37,8 +46,8 @@ labels = data.loc[:, 'centroid']
 
 
 # Plot of all data correlation matrix
-plt.figure(figsize=(40, 40))
 sns.heatmap(data.corr(), annot=True, cmap='RdYlGn')
+plt.show()
 
 # Normalization of the features
 minmax = MinMaxScaler()
@@ -47,18 +56,23 @@ features = minmax.fit_transform(features)
 # Split data into train and test (random split in every run)
 x_train, x_test, y_train, y_test = train_test_split(features, labels, test_size=0.2)
 
+
 # Initialize model
-linear_regr = LinearRegression()
+models = [(LinearRegression(), 'Linear Regression'), (MLPRegressor(random_state=1, max_iter=500), 'MLP Regression')]
 
-scoring = {'r2_scr': 'r2',
-           'NRMSE_scr': 'neg_root_mean_squared_error'}
+for mod in models:
+    scoring = {'r2_scr': 'r2',
+               'NRMSE_scr': 'neg_root_mean_squared_error',
+               'max_error_scr': 'max_error',
+               'prediction_plot_scorer': prediction_plot_scorer
+               }
 
+    # Cross-validation
+    scores = cross_validate(mod[0], x_train, y_train, scoring=scoring, return_estimator=False, cv=10)
+    print(f'Training and Evaluating {mod[1]}!!')
 
-# Cross-validation
-scores = cross_validate(linear_regr, x_train, y_train, scoring=scoring, return_estimator=False, cv=10)
-
-# Mean scores calculation
-for k, v in scores.items():
-    if k == 'test_conf_mtx':
-        continue
-    print(f'{k}: {mean(v)}')
+    # Mean scores calculation
+    for k, v in scores.items():
+        if k == 'test_conf_mtx':
+            continue
+        print(f'{k}: {mean(v)}')
